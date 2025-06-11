03ee5af0a6ba231483d7e5989bd171e895ba1655
Heitor Vieira Mello <heitor29_@hotmail.com>
2025-06-10 23:09:30 -0300

    Add nested rate-limiting configuration and corresponding environment variables

--- internal/middleware/ratelimit/config.go
+++ internal/middleware/ratelimit/config.go
@@ -3,6 +3,11 @@ package ratelimit
 type Config struct {
 	RequestsPerMinute int
 	BurstSize         int
+	RateLimit         struct {
+		Enabled           bool `env:"RATE_LIMIT_ENABLED" envDefault:"true"`
+		RequestsPerMinute int  `env:"RATE_LIMIT_REQUESTS_PER_MINUTE" envDefault:"60"`
+		BurstSize         int  `env:"RATE_LIMIT_BURST_SIZE" envDefault:"20"`
+	}
 }
 
 func NewConfig(requestsPerMinute, burstSize int) Config {
